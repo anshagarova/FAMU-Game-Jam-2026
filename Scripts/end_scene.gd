1652d8312285f1867334166bf258b04fd0b960b5
Eliška Szopová <Shopingjs@gmail.com>
2026-02-21 17:39:59 +0100

    work on scenes

---
+++ Scripts/end_scene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+func _on_file_selected(path) -> void:
+	pass
+	
+
+func _on_save_pressed() -> void:
+	var file_dialog = $FileDialog
+	file_dialog.mode = FileDialog.FILE_MODE_SAVE_FILE
+	file_dialog.connect("file_selected", Callable(self, "_on_file_selected"))
+	file_dialog.popup_centered_ratio()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Sprite2D.texture = ImageTexture.create_from_image(Global.final_image)
+	$SaveImageButton.pressed.connect(_on_save_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Scripts/finish_game.gd
+++ Scripts/finish_game.gd
@@ -4,6 +4,24 @@ func _ready():
 	$HBoxContainer/FinishButton.pressed.connect(_on_finish_pressed)
 
 func _on_finish_pressed():
+	var brush = %paintingScene/Brush
+	var canvas = brush._canvas
+	print("Canvas size", canvas.get_size())
+	canvas.save_png("test.png")
+	var face_image = load("res://Assets/blank_faces/01.png").get_image()
+	face_image.resize(1080, 1080)
+	var center_start = (canvas.get_width() / 2) - (face_image.get_width() / 2)
+	var face_image_new = face_image.duplicate()
+	face_image_new.blend_rect(canvas, Rect2i(center_start, 0, face_image.get_width(), face_image.get_height()), Vector2(0, 0))
+	var final_image = Image.create_empty(1080, 1080, false, Image.FORMAT_RGBA8)
+	final_image.blit_rect_mask(face_image_new, face_image, Rect2i(0, 0, 1080, 1080), Vector2(0, 0))
+	# canvas.blend_rect(face_image, Rect2i(0, 0, face_image.get_width(), face_image.get_height()), Vector2(center_start, 0))
+	# canvas.save_png("test_blend.png")
+	final_image.save_png("test_blend.png")
+	var should_be_image = load("res://Assets/with_makeup/01.png").get_image()
+	var image_metrics = final_image.compute_image_metrics(should_be_image, false)
+	print(image_metrics)
+	Global.final_image = final_image
 	var scene_path = "res://Scenes/End_scene.tscn"
 	print("Button pressed → Changing to:", scene_path)
 	get_tree().change_scene_to_file(scene_path)
