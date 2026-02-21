d8388d6da310dc0199613710789685545644de31
Eliška Szopová <Shopingjs@gmail.com>
2026-02-21 21:50:13 +0100

    add bathroom bg, fix crash

--- Scripts/finish_game.gd
+++ Scripts/finish_game.gd
@@ -35,20 +35,28 @@ func _ready():
 	$Hair6Button.pressed.connect(_on_hair_six_pressed)
 
 func _on_finish_pressed():
-	var brush = %paintingScene/Brush
+	var brush = %paintingScene/ThinBrush
 	var canvas = brush._canvas
+	var brush_medium = %paintingScene/SoftBrush
+	var canvas_medium = brush_medium._canvas
+	var brush_soft = %paintingScene/MediumBrush
+	var canvas_soft = brush_soft._canvas
 	print("Canvas size", canvas.get_size())
-	canvas.save_png("test.png")
 	var face_image = load("res://Assets/blank_faces/01.png").get_image()
 	face_image.resize(1080, 1080)
 	var center_start = (canvas.get_width() / 2) - (face_image.get_width() / 2)
 	var face_image_new = face_image.duplicate()
+	face_image_new.blend_rect(canvas_medium, Rect2i(center_start, 56, face_image.get_width(), face_image.get_height()), Vector2(0, 0))
+	face_image_new.blend_rect(canvas_soft, Rect2i(center_start, 56, face_image.get_width(), face_image.get_height()), Vector2(0, 0))
 	face_image_new.blend_rect(canvas, Rect2i(center_start, 56, face_image.get_width(), face_image.get_height()), Vector2(0, 0))
 	var final_image = Image.create_empty(1080, 1080, false, Image.FORMAT_RGBA8)
 	final_image.blit_rect_mask(face_image_new, face_image, Rect2i(0, 0, 1080, 1080), Vector2(0, 0))
 	# canvas.blend_rect(face_image, Rect2i(0, 0, face_image.get_width(), face_image.get_height()), Vector2(center_start, 0))
 	# canvas.save_png("test_blend.png")
 	final_image.save_png("test_blend.png")
+	var hair_image = load("res://Assets/hair/01.png").get_image()
+	hair_image.resize(1080, 1080)
+	final_image.blend_rect(hair_image, Rect2i(0, 0, hair_image.get_width(), hair_image.get_height()), Vector2(0, 0))
 	var should_be_image = load("res://Assets/with_makeup/01.png").get_image()
 	var image_metrics = final_image.compute_image_metrics(should_be_image, false)
 	print(image_metrics)
