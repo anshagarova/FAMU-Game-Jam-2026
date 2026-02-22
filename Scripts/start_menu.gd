de5d683019d51f9476c95c607156160b0cc41177
withthelemons <with.the.lemons@gmail.com>
2026-02-22 01:15:58 +0100

    first dialogue
    yay!

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -2,8 +2,13 @@ extends Control
 
 func _ready():
 	$VBoxContainer/PlayButton.pressed.connect(_on_play_button_pressed)
+	
+func _on_dialogue_ended(flags):
+	var scene_path = "res://Scenes/Choose_player_menu.tscn"
+	get_tree().change_scene_to_file(scene_path)
+	
 
 func _on_play_button_pressed():
-	var scene_path = "res://Scenes/Choose_player_menu.tscn"
-	print("Button pressed → Changing to:", scene_path)
-	get_tree().change_scene_to_file(scene_path)
+	var resource = load("res://Assets/main_dialogue.dialogue")
+	DialogueManager.show_dialogue_balloon(resource, "start")
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
